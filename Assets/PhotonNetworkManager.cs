bb6c63330d61fc8d82a3341afb22ad41c6f5fb51
valentincognito <valentin.vannay@gmail.com>
2017-07-05 11:48:31 +0900

    Spawn player in a room

--- Assets/PhotonNetworkManager.cs
+++ Assets/PhotonNetworkManager.cs
@@ -6,17 +6,27 @@ using UnityEngine.UI;
 public class PhotonNetworkManager : Photon.MonoBehaviour {
 
     [SerializeField] private Text connectText;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject lobbyCamera;
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         PhotonNetwork.ConnectUsingSettings("v1.0");
 	}
 
-    public virtual void OnConnectedToMaster()
+    public virtual void OnJoinedLobby()
     {
-        Debug.Log("connected");
+        Debug.Log("lobby joined");
+        //RoomOptions roomOptions = new RoomOptions();
+        PhotonNetwork.JoinOrCreateRoom("New", null, null);
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(player.name, spawnPoint.position, spawnPoint.rotation, 0);
+        lobbyCamera.SetActive(false);
     }
 	
 	// Update is called once per frame
