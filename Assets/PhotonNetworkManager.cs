b58990a1c887d15800f33392c2edac11613f7a67
valentincognito <valentin.vannay@gmail.com>
2017-07-05 11:15:57 +0900

    Add photon network manager

---
+++ Assets/PhotonNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : Photon.MonoBehaviour {
+
+    [SerializeField] private Text connectText;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1.0");
+	}
+
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("connected");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
